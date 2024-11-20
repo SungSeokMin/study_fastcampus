b4f9e8ff34df0f6ba5a635504b06616ea91b2072
sungseokmin <jkl154527@gmail.com>
2024-11-20 14:43:28 +0900

    Ch 3.4-6 controller에서 service로 전환하기

--- nestjs_part1/src/app.controller.ts
+++ nestjs_part1/src/app.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller('movie')
@@ -10,52 +19,23 @@ export class AppController {
     return this.appService.getManyMovies(title);
   }
 
-  // @Get(':id')
-  // getMovie(@Param('id') id: string) {
-  //   const movie = this.movies.find((movie) => movie.id === +id);
+  @Get(':id')
+  getMovie(@Param('id') id: string) {
+    return this.appService.getMovieById(+id);
+  }
 
-  //   if (!movie) {
-  //     throw new NotFoundException('존재하지 않는 ID 값의 영화입니다.');
-  //   }
+  @Post()
+  postMovie(@Body('title') title: string) {
+    return this.appService.createMovie(title);
+  }
 
-  //   return movie;
-  // }
+  @Patch(':id')
+  patchMovie(@Param('id') id: string, @Body('title') title: string) {
+    return this.appService.updateMovie(+id, title);
+  }
 
-  // @Post()
-  // postMovie(@Body('title') title: string) {
-  //   const movie: Movie = {
-  //     id: this.idCounter++,
-  //     title,
-  //   };
-
-  //   this.movies.push(movie);
-
-  //   return movie;
-  // }
-
-  // @Patch(':id')
-  // patchMovie(@Param('id') id: string, @Body('title') title: string) {
-  //   const movie = this.movies.find((movie) => movie.id === +id);
-
-  //   if (!movie) {
-  //     throw new NotFoundException('존재하지 않는 ID 값의 영화입니다.');
-  //   }
-
-  //   Object.assign(movie, { title });
-
-  //   return movie;
-  // }
-
-  // @Delete(':id')
-  // deleteMovie(@Param('id') id: string) {
-  //   const movieIndex = this.movies.findIndex((movie) => movie.id === +id);
-
-  //   if (movieIndex === -1) {
-  //     throw new NotFoundException('존재하지 않는 ID 값의 영화입니다.');
-  //   }
-
-  //   this.movies.splice(movieIndex, 1);
-
-  //   return id;
-  // }
+  @Delete(':id')
+  deleteMovie(@Param('id') id: string) {
+    return this.appService.deleteMovie(+id);
+  }
 }
