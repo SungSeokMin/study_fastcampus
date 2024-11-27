c03ad0def30d63d59f7d196cecd13aa611ec3e62
sungseokmin <jkl154527@gmail.com>
2024-11-27 16:26:27 +0900

    DataSource 정의하고 환경변수 사용해보기

--- nestjs_part1/src/app.module.ts
+++ nestjs_part1/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { MovieModule } from './movie/movie.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MovieModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: process.env.DB_TYPE as 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [],
+      synchronize: true,
+    }),
+    MovieModule,
+  ],
   controllers: [],
   providers: [],
 })
