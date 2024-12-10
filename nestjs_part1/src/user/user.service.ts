d16d114451d7cea271881105919d353360110b80
sungseokmin <jkl154527@gmail.com>
2024-12-10 15:58:54 +0900

    User Module 작업하기

--- nestjs_part1/src/user/user.service.ts
+++ nestjs_part1/src/user/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { User } from './entitiy/user.entity';
+import { User } from './entity/user.entity';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
