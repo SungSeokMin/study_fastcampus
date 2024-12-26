866bec3ac3cd170296fbe8cc9ff9f64870f64d96
sungseokmin <jkl154527@gmail.com>
2024-12-26 15:53:20 +0900

    query failed exception filter 구현

--- nestjs_part1/src/app.module.ts
+++ nestjs_part1/src/app.module.ts
@@ -22,6 +22,7 @@ import { AuthGuard } from './auth/guard/auth.guard';
 import { RBACGuard } from './auth/guard/rbac.guard';
 import { ResponseTimeInterceptor } from './common/interceptor/response-time.interceptor';
 import { ForbiddenExceptionFilter } from './common/filter/forbidden.filter';
+import { QueryFailedExceptionFilter } from './common/filter/query-failed.filter';
 
 @Module({
   imports: [
@@ -75,6 +76,10 @@ import { ForbiddenExceptionFilter } from './common/filter/forbidden.filter';
     {
       provide: APP_FILTER,
       useClass: ForbiddenExceptionFilter,
+    },
+    {
+      provide: APP_FILTER,
+      useClass: QueryFailedExceptionFilter,
     },
   ],
 })
