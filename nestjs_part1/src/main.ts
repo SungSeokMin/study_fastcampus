8463ffd57890a992b8373ce69ed193a4293cdf8c
sungseokmin <jkl154527@gmail.com>
2024-12-28 13:36:45 +0900

    Class Transformer

--- nestjs_part1/src/main.ts
+++ nestjs_part1/src/main.ts
@@ -5,7 +5,12 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // 정의하지 않는 요소를 request해도 무시한다.
+      forbidNonWhitelisted: true, // 정의하지 않은 요소가 request에 있으면 에러를 발생시킨다.
+    }),
+  );
 
   await app.listen(process.env.PORT ?? 3000);
 }
