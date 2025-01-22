c426cee9af46ef7b5b47dbfdb80e2a9ad2ed8f1b
sungseokmin <jkl154527@gmail.com>
2025-01-22 10:25:18 +0900

    Genre Module Unit Test

--- nestjs_part1/src/genre/genre.service.ts
+++ nestjs_part1/src/genre/genre.service.ts
@@ -12,22 +12,22 @@ export class GenreService {
     private readonly genreRepository: Repository<Genre>,
   ) {}
 
+  async create(createGenreDto: CreateGenreDto) {
+    return this.genreRepository.save(createGenreDto);
+  }
+
   findAll() {
     return this.genreRepository.find();
   }
 
-  findOne(id: number) {
-    return this.genreRepository.findOne({ where: { id } });
-  }
+  async findOne(id: number) {
+    const genre = await this.genreRepository.findOne({ where: { id } });
 
-  async create(createGenreDto: CreateGenreDto) {
-    // const genre = await this.genreRepository.findOne({ where: { name: createGenreDto.name } });
+    if (!genre) {
+      throw new NotFoundException('존재하지 않는 ID의 장르입니다.');
+    }
 
-    // if (genre) {
-    //   throw new NotFoundException('이미 존재하는 장르입니다.');
-    // }
-
-    return this.genreRepository.save(createGenreDto);
+    return genre;
   }
 
   async update(id: number, updateGenreDto: UpdateGenreDto) {
