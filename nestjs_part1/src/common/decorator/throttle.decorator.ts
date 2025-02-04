74afb46aba529f21e33373521974924125002f50
sungseokmin <jkl154527@gmail.com>
2025-02-04 15:55:27 +0900

    remove nesetjs_part1

--- nestjs_part1/src/common/decorator/throttle.decorator.ts
+++
@@ -1,8 +0,0 @@
-import { Reflector } from '@nestjs/core';
-
-export interface ThrottleOptions {
-  count: number;
-  unit: 'minute';
-}
-
-export const Throttle = Reflector.createDecorator<ThrottleOptions>();
--- nestjs_part1/src/worker/worker.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ThumbnailGenerationProcess } from './thumbnail-generation.worker';
-
-@Module({
-  providers: [ThumbnailGenerationProcess],
-})
-export class WorkerModule {}
